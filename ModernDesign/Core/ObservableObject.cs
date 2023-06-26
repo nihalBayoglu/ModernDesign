e14dcb3fd06b8b3b4f8e67a4b04f007d2a3e4c03
nihal <nihalbayoglu@gmail.com>
2023-06-26 23:33:10 +0300

    Developed to Core Layer and MainWindow.

--- ModernDesign/Core/ObservableObject.cs
+++ ModernDesign/Core/ObservableObject.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,10 @@ namespace ModernDesign.Core
     class ObservableObject : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
